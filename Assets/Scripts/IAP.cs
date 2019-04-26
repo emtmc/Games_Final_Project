eed1e6a8727d621bf940ab185f54d71cd196d836
Eoin McGrath <eoin.mcg96@gmail.com>
2019-04-26 16:19:19 +0100

    Unity Ads - Working Admob - Version Issues - not working Achievements - Working Leaderboards - Working Facebook - Working IAP - working

---
+++ Assets/Scripts/IAP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PurchaseComplete(UnityEngine.Purchasing.Product product)
+    {
+        Debug.Log("Purchase Complete");
+        UIScript.Instance.UnlockGameOver();
+        UIScript.Instance.UnlockFullScore();
+    }
+
+    public void PurchaseFailed(UnityEngine.Purchasing.Product product,
+        UnityEngine.Purchasing.PurchaseFailureReason failure)
+    {
+        Debug.Log("Purchase Failed");
+    }
+}
